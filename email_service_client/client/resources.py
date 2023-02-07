02543669b5ed94d3f34251e613cc8f41bcc0b10c
Mridula Prabhu <mridula.prabhu@plivo.com>
2023-02-07 16:27:36 +0530

    feat: implement hooks to raise HTTP errors

--- email_service_client/client/resources.py
+++ email_service_client/client/resources.py
@@ -9,14 +9,7 @@ from email_service_client.client.types import CreateEmailIdentityPayload, EmailI
 from functools import wraps
 
 
-def raise_http_error(f):
-    @wraps(f)
-    def wrapper(*args, **kwds):
-        response: requests.Response = f(*args, **kwds)
-        if response.status_code != 200:
-            raise requests.exceptions.HTTPError(response.status_code, response.json())
-        return response.json()
-    return wrapper
+
 
   
 
@@ -26,6 +19,7 @@ API_BASE_URL = f"{settings.EMAIL_SERVICE_API_HOST}/api/v1"
 class APIResource:
     def __init__(self, session: requests.Session) -> None:
         self._session = session
+        self._session.hooks = {"response": lambda r, *args, **kwargs: r.raise_for_status()}
 
 
 class _EmailIdentity(APIResource):
@@ -36,15 +30,15 @@ class _EmailIdentity(APIResource):
         self.resource_url = f"{API_BASE_URL}/email-identities"
 
 
-    @raise_http_error
-    def list(self) -> List[EmailIdentity]:
-        return self._session.get(self.resource_url) #type: ignore
-    
-    @raise_http_error
-    def get(self, id: int) -> EmailIdentity:
-        return self._session.get(f"{self.resource_url}/{id}") #type: ignore
 
-    @raise_http_error
+    def list(self) -> List[EmailIdentity]:
+        return self._session.get(self.resource_url).json()
+    
+
+    def get(self, id: int) -> EmailIdentity:
+        return self._session.get(f"{self.resource_url}/{id}").json()
+
+
     def create(self, data: CreateEmailIdentityPayload) -> dict:
 
         encoded_data = requests_toolbelt.MultipartEncoder(fields=data)
@@ -53,25 +47,25 @@ class _EmailIdentity(APIResource):
             data=encoded_data,
             headers={"Content-Type": encoded_data.content_type},
         )
-        return self._session.post(self.resource_url) #type: ignore
+        return self._session.post(self.resource_url).json()
 
-    @raise_http_error
+
     def configure_sending(self, id: int) -> dict:
-        return self._session.post(f"{self.resource_url}/{id}/configure/sending") #type: ignore
+        return self._session.post(f"{self.resource_url}/{id}/configure/sending").json()
 
-    @raise_http_error
+
     def deconfigure_sending(self, id: int) -> dict:
-        return self._session.post(f"{self.resource_url}/{id}/deconfigure/sending") #type: ignore
+        return self._session.post(f"{self.resource_url}/{id}/deconfigure/sending").json()
     
-    @raise_http_error
+
     def delete(self, id: int) -> dict:
-        return self._session.post(f"{self.resource_url}/{id}/delete") #type: ignore
+        return self._session.post(f"{self.resource_url}/{id}/delete").json()
 
-    @raise_http_error
+
     def get_sending_dns_records(self, id: int) -> dict:
-        return self._session.get(f"{self.resource_url}/{id}/dns-records/sending") #type: ignore
+        return self._session.get(f"{self.resource_url}/{id}/dns-records/sending").json()
 
-    @raise_http_error
+
     def get_sending_verification_status(self, id: int) -> dict:
-        return self._session.get(f"{self.resource_url}/{id}/verification-status/sending") #type: ignore
+        return self._session.get(f"{self.resource_url}/{id}/verification-status/sending").json()
 
