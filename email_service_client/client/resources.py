c5676d8ca6a9c72ad2e13382c6558b777784bf96
mridula-plivo <95842641+mridula-plivo@users.noreply.github.com>
2023-02-07 16:35:04 +0530

    Raise exception if response is not 200 (#3)
    * feat: raise exception if response is not 200
    
    * feat: implement hooks to raise HTTP errors
    
    * chore: run pre-commit

--- email_service_client/client/resources.py
+++ email_service_client/client/resources.py
@@ -12,6 +12,7 @@ API_BASE_URL = f"{settings.EMAIL_SERVICE_API_HOST}/api/v1"
 class APIResource:
     def __init__(self, session: requests.Session) -> None:
         self._session = session
+        self._session.hooks = {"response": lambda r, *args, **kwargs: r.raise_for_status()}
 
 
 class _EmailIdentity(APIResource):
@@ -24,10 +25,11 @@ class _EmailIdentity(APIResource):
     def list(self) -> List[EmailIdentity]:
         return self._session.get(self.resource_url).json()
 
-    def get(self, id) -> EmailIdentity:
+    def get(self, id: int) -> EmailIdentity:
         return self._session.get(f"{self.resource_url}/{id}").json()
 
-    def create(self, data: CreateEmailIdentityPayload):
+    def create(self, data: CreateEmailIdentityPayload) -> dict:
+
         encoded_data = requests_toolbelt.MultipartEncoder(fields=data)
         self._session.post(
             self.resource_url,
@@ -35,3 +37,18 @@ class _EmailIdentity(APIResource):
             headers={"Content-Type": encoded_data.content_type},
         )
         return self._session.post(self.resource_url).json()
+
+    def configure_sending(self, id: int) -> dict:
+        return self._session.post(f"{self.resource_url}/{id}/configure/sending").json()
+
+    def deconfigure_sending(self, id: int) -> dict:
+        return self._session.post(f"{self.resource_url}/{id}/deconfigure/sending").json()
+
+    def delete(self, id: int) -> dict:
+        return self._session.post(f"{self.resource_url}/{id}/delete").json()
+
+    def get_sending_dns_records(self, id: int) -> dict:
+        return self._session.get(f"{self.resource_url}/{id}/dns-records/sending").json()
+
+    def get_sending_verification_status(self, id: int) -> dict:
+        return self._session.get(f"{self.resource_url}/{id}/verification-status/sending").json()
--- email_service_client/tests/unit_tests/email_identity_resource_tests.py
+++ email_service_client/tests/unit_tests/email_identity_resource_tests.py
@@ -10,7 +10,7 @@ class EmailIdentityResourceTests(ResourceTestCase):
         return super().setUp()
 
     def test_list_email_identities(self):
-        dummy_list = MagicMock()
+        dummy_list = MagicMock(status_code=200)
         self.requests_get_patch.return_value = dummy_list
         email_service_client = EmailServiceClient(username="dummy", password="dummy")
         self.assertEqual(email_service_client.EmailIdentity.list(), dummy_list.json())
@@ -19,7 +19,8 @@ class EmailIdentityResourceTests(ResourceTestCase):
         )
 
     def test_get_email_identity(self):
-        dummy_list = MagicMock()
+
+        dummy_list = MagicMock(status_code=200)
         self.requests_get_patch.return_value = dummy_list
         email_service_client = EmailServiceClient(username="dummy", password="dummy")
         self.assertEqual(email_service_client.EmailIdentity.get(23434), dummy_list.json())
@@ -28,7 +29,8 @@ class EmailIdentityResourceTests(ResourceTestCase):
         )
 
     def test_create_email_identity(self):
-        dummy_response = MagicMock()
+
+        dummy_response = MagicMock(status_code=200)
         self.requests_post_patch.return_value = dummy_response
         email_service_client = EmailServiceClient(username="dummy", password="dummy")
         self.assertEqual(
@@ -37,4 +39,64 @@ class EmailIdentityResourceTests(ResourceTestCase):
         )
         self.requests_post_patch.assert_called_with(
             f"{EMAIL_SERVICE_API_HOST}/api/v1/email-identities"
+        )
+
+    def test_configure_sending_email_identity(self):
+        dummy_response = MagicMock(status_code=200)
+        self.requests_post_patch.return_value = dummy_response
+        email_service_client = EmailServiceClient(username="dummy", password="dummy")
+        self.assertEqual(
+            email_service_client.EmailIdentity.configure_sending(id=19),
+            dummy_response.json(),
+        )
+        self.requests_post_patch.assert_called_with(
+            f"{EMAIL_SERVICE_API_HOST}/api/v1/email-identities/19/configure/sending"
+        )
+
+    def test_deconfigure_sending_email_identity(self):
+        dummy_response = MagicMock(status_code=200)
+        self.requests_post_patch.return_value = dummy_response
+        email_service_client = EmailServiceClient(username="dummy", password="dummy")
+        self.assertEqual(
+            email_service_client.EmailIdentity.deconfigure_sending(id=19),
+            dummy_response.json(),
+        )
+        self.requests_post_patch.assert_called_with(
+            f"{EMAIL_SERVICE_API_HOST}/api/v1/email-identities/19/deconfigure/sending"
+        )
+
+    def test_delete_sending_email_identity(self):
+        dummy_response = MagicMock(status_code=200)
+        self.requests_post_patch.return_value = dummy_response
+        email_service_client = EmailServiceClient(username="dummy", password="dummy")
+        self.assertEqual(
+            email_service_client.EmailIdentity.delete(id=19),
+            dummy_response.json(),
+        )
+        self.requests_post_patch.assert_called_with(
+            f"{EMAIL_SERVICE_API_HOST}/api/v1/email-identities/19/delete"
+        )
+
+    def test_get_sending_dns_records(self):
+        dummy_response = MagicMock(status_code=200)
+        self.requests_get_patch.return_value = dummy_response
+        email_service_client = EmailServiceClient(username="dummy", password="dummy")
+        self.assertEqual(
+            email_service_client.EmailIdentity.get_sending_dns_records(id=19),
+            dummy_response.json(),
+        )
+        self.requests_get_patch.assert_called_with(
+            f"{EMAIL_SERVICE_API_HOST}/api/v1/email-identities/19/dns-records/sending"
+        )
+
+    def test_get_sending_verification_status(self):
+        dummy_response = MagicMock(status_code=200)
+        self.requests_get_patch.return_value = dummy_response
+        email_service_client = EmailServiceClient(username="dummy", password="dummy")
+        self.assertEqual(
+            email_service_client.EmailIdentity.get_sending_verification_status(id=19),
+            dummy_response.json(),
+        )
+        self.requests_get_patch.assert_called_with(
+            f"{EMAIL_SERVICE_API_HOST}/api/v1/email-identities/19/verification-status/sending"
         )
